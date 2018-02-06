2d2f904602741d764c50a775e212eff04597334f
xiaol <xiaol@tenzont.com>
2018-02-06 18:48:13 +0800

    增强文章保存的容错能力

--- src/main/java/me/leiho/blog/services/impls/ArticleServiceImpl.java
+++ src/main/java/me/leiho/blog/services/impls/ArticleServiceImpl.java
@@ -3,11 +3,14 @@ package me.leiho.blog.services.impls;
 import com.sun.xml.internal.rngom.parse.host.Base;
 import me.leiho.blog.entities.BaseResult;
 import me.leiho.blog.entities.XArticle;
+import me.leiho.blog.entities.XArticleTag;
 import me.leiho.blog.mappers.XArticleMapper;
+import me.leiho.blog.mappers.XArticleTagMapper;
 import me.leiho.blog.mappers.XArticleTypeMapper;
 import me.leiho.blog.services.ArticleService;
 import me.leiho.blog.utils.JsonUtil;
 import me.leiho.blog.vos.ArticleWriteVO;
+import me.leiho.blog.vos.TagsVO;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +18,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.entity.Example;
 
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+
 import static me.leiho.blog.enums.ResultCode.*;
 
 /**
  * @Author: 萧大侠
- * @Description:
+ * @Description: 文章服务
  * @Date: Create in 18:10 2018/2/5
  * @Contact: yesxiaolei@outlook.com
  */
@@ -30,6 +37,8 @@ public class ArticleServiceImpl implements ArticleService {
     private XArticleMapper xArticleMapper;
     @Autowired
     private XArticleTypeMapper xArticleTypeMapper;
+    @Autowired
+    private XArticleTagMapper xArticleTagMapper;
     public BaseResult saveOrAnnounceArticle(String article,Boolean announce){
         //json反序列化
         ArticleWriteVO articleWriteVO = JsonUtil.json2pojo(article, ArticleWriteVO.class);
@@ -59,10 +68,42 @@ public class ArticleServiceImpl implements ArticleService {
             //感想过长
             return new BaseResult(FAILED_ARTICLE_FEELING_TOLONG);
         }
-        //文章类型不存在
-
-        //文章标签不存在
-        //
+        if (xArticleTypeMapper.selectByPrimaryKey(articleWriteVO.getType())==null){
+            //文章类型不存在
+            return new BaseResult(FAILED_ARTICLE_TYPE_NOT_EXIST);
+        }
+        if (checkTagFailed(articleWriteVO.getTags())){
+            //文章标签不存在
+            return new BaseResult(FAILED_ARTICLE_TAG_NOT_EXIST);
+        }
+        //写入数据库
+        if (announce){
+            //发布
+        }else {
+            //保存
+        }
         return new BaseResult(SUCCESS);
+    }
+    private Boolean checkTagFailed(String tags){
+        if (StringUtils.isBlank(tags)){
+            return false;
+        }
+        tags = tags.trim();
+        tags.replace("，",",");
+        if (tags.indexOf(",")>0){
+            //多tag
+            String[] tagList = tags.split(",");
+            for (String tag:tagList){
+                if (xArticleTagMapper.selectByPrimaryKey(tag)==null){
+                    return true;
+                }
+            }
+        }else {
+            //单tag
+            if (xArticleTagMapper.selectByPrimaryKey(tags)==null){
+                return true;
+            }
+        }
+        return false;
     }
 }
